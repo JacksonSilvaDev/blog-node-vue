cb51e449665053505526cbb218cef92d52863c86
Jackson <jackson.rodrigues9@hotmail.com>
2019-06-11 17:40:34 -0300

    Criado o component post

--- blog-client/src/components/Home.vue
+++ blog-client/src/components/Home.vue
@@ -4,51 +4,19 @@
       <v-flex xs12>
         <h1 display-4>Home</h1>
       </v-flex>
-
-      <v-flex xs12 sm6 md6>
-        <v-hover>
-          <v-card
-            slot-scope="{ hover }"
-            :class="`elevation-${hover ? 12 : 2}`"
-            color="darken-5"
-            class="green-custom white--text"
-          >
-            <v-layout>
-              <v-flex xs5>
-                <v-img
-                  src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
-                  height="200px"
-                  contain
-                ></v-img>
-              </v-flex>
-              <v-flex xs7>
-                <v-card-title primary-title>
-                  <div>
-                    <div class="headline">Supermodel</div>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu lorem vel enim consectetur molestie a vel sem. Etiam tincidunt mi in massa dictum, eu egestas ipsum iaculis. Maecenas orci nibh, auctor quis sapien at, eleifend feugiat erat. Praesent vitae finibus sapien, semper egestas lectus. Duis euismod vehicula convallis. Etiam quis ligula pulvinar, feugiat arcu a, dapibus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst. Phasellus suscipit aliquam sapien, id tincidunt mauris bibendum quis. Pellentesque et cursus nibh.</div>
-                  </div>
-                </v-card-title>
-              </v-flex>
-            </v-layout>
-            <v-divider light></v-divider>
-            <v-card-actions class="pa-3">
-              23 de outubro de 2019 - Jackson
-              <v-spacer></v-spacer>
-              <v-rating background-color="orange lighten-3" color="orange" v-model="rating"></v-rating>
-            </v-card-actions>
-          </v-card>
-        </v-hover>
-      </v-flex>
+      <PostHome/>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import PostHome from "./PostHomeComponent";
 export default {
   data: () => {
-    return {
-      rating: 3
-    };
+    return {};
+  },
+  components: {
+    PostHome
   }
 };
 </script>
@@ -56,5 +24,6 @@ export default {
 <style>
 .v-card {
   border-radius: 20px;
+  cursor: pointer;
 }
 </style>
---
+++ blog-client/src/components/PostHomeComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-flex xs12 sm6 md6>
+    <v-hover>
+      <v-card
+        slot-scope="{ hover }"
+        :class="`elevation-${hover ? 12 : 2}`"
+        color="darken-5"
+        class="green-custom white--text"
+      >
+        <v-layout>
+          <v-flex xs5>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg" height="200px" contain></v-img>
+          </v-flex>
+          <v-flex xs7>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Supermodel</div>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu lorem vel enim consectetur molestie a vel sem. Etiam tincidunt mi in massa dictum, eu egestas ipsum iaculis. Maecenas orci nibh, auctor quis sapien at, eleifend feugiat erat. Praesent vitae finibus sapien, semper egestas lectus. Duis euismod vehicula convallis. Etiam quis ligula pulvinar, feugiat arcu a, dapibus ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst. Phasellus suscipit aliquam sapien, id tincidunt mauris bibendum quis. Pellentesque et cursus nibh.</div>
+              </div>
+            </v-card-title>
+          </v-flex>
+        </v-layout>
+        <v-divider light></v-divider>
+        <v-card-actions class="pa-3">
+          23 de outubro de 2019 - Jackson
+          <v-spacer></v-spacer>
+          <v-rating background-color="orange lighten-3" color="orange" v-model="rating"></v-rating>
+        </v-card-actions>
+      </v-card>
+    </v-hover>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rating: 3
+    };
+  }
+};
+</script>
+
+<style>
+</style>
